9a3b27326d49f9a353beda111da90bf88820c4ea
taskpul <romtietz@gmail.com>
2025-10-29 09:31:45 +0100

    Ensure Welcome module file is loaded

--- fluent-community/Modules/modules_init.php
+++ fluent-community/Modules/modules_init.php
@@ -1,6 +1,17 @@
 <?php
 
 add_action('fluent_community/portal_loaded', function ($app) {
+    // Ensure module classes are available when Composer's classmap is authoritative
+    $moduleFiles = [
+        __DIR__ . '/Welcome/WelcomeModule.php',
+    ];
+
+    foreach ($moduleFiles as $moduleFile) {
+        if (is_readable($moduleFile)) {
+            require_once $moduleFile;
+        }
+    }
+
     (new \FluentCommunity\Modules\FeaturesHandler())->register($app);
 
     // Load the Integrations
